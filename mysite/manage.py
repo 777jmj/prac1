62cb48ef1713329857699e74c568d4a84767292b
777jmj <777jmj@naver.com>
2019-08-25 21:57:24 +0900

    bugfix

--- mysite/manage.py
+++ mysite/manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,6 +16,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-    
+
 if __name__ == '__main__':
-     main()
+    main()
